9907c0c727f0e45e3518c3ce041e8a31be7bfc90
Felipe Rudnik <82181914+zulark@users.noreply.github.com>
2024-05-30 14:22:50 -0300

    ajuste na criação de funcionarios
    adicionado salario base

--- src/api/funcionarios/createEmployee.php
+++ src/api/funcionarios/createEmployee.php
@@ -5,7 +5,7 @@ include '../db_connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
 
-    if (!isset($data['nome'], $data['email'], $data['senha'], $data['filial_id'])) {
+    if (!isset($data['nome'], $data['email'], $data['senha'], $data['filial_id'], $data['salario_base'])) {
         echo json_encode(['error' => 'Campos não preenchidos']);
         http_response_code(400);
         exit;
@@ -14,11 +14,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = mysqli_real_escape_string($conn, $data['email']);
     $senha = mysqli_real_escape_string($conn, $data['senha']);
     $senhaCriptografada = password_hash($senha, PASSWORD_DEFAULT);
-    $salario_base = mysqli_real_escape_string($conn, (string) (float) $data['salario_base']);
     $filial_id = intval($data['filial_id']);
+    $salario_base = mysqli_real_escape_string($conn, (string) (float) $data['salario_base']);
 
-    $sql = "INSERT INTO funcionarios (nome, email, senha, filial_id) 
-            VALUES ('$nome', '$email', '$senhaCriptografada', '$filial_id')";
+    $sql = "INSERT INTO funcionarios (nome, email, senha, filial_id, salario_base) 
+            VALUES ('$nome', '$email', '$senhaCriptografada', '$filial_id', '$salario_base')";
 
     if ($conn->query($sql) === TRUE) {
         $new_employee = [
@@ -26,6 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'email' => $email,
             'senha' => $senhaCriptografada,
             'filial_id' => $filial_id,
+            'salario_base' => $salario_base
         ];
         echo json_encode(['success' => true, 'message' => 'Funcionário criado com sucesso', 'employee' => $new_employee]);
     } else {
