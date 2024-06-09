85a15207bb95b098ddd23b60e42125c933761f56
Felipe Rudnik <82181914+zulark@users.noreply.github.com>
2024-06-09 13:52:23 -0300

    ajustes css e classes

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/2598/2598702.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
 </head>
@@ -17,7 +18,6 @@
     <?php
     include ('..\..\components\sidebarUser.php');
     ?>
-
     <div class="container-fluid carousel">
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
             <div class="carousel-indicators">
