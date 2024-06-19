22bd686267b2ac2ae9ac3b120113e47c4f0c5d2c
Felipe Rudnik <82181914+zulark@users.noreply.github.com>
2024-06-19 19:47:00 -0300

    ajuste session setor e banco

--- src/api/db_connection.php
+++ src/api/db_connection.php
@@ -2,12 +2,10 @@
 $hostName = "localhost";
 $username = "root";
 $password = "";
-$database = "butacabox_v3";
+$database = "butacabox_v4";
 
-// Create connection
 $conn = new mysqli($hostName, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
--- src/pages/login-funcionario/protect.php
+++ src/pages/login-funcionario/protect.php
@@ -8,8 +8,7 @@ $accepted_roles = [1, 2];
 if (!isset($_SESSION['id_funcionario'])) {
     die("Você não pode acessar esta página porque não está logado.<p><a href=\"http://127.0.0.1/ButacaBox/ButacaBox/src/pages/login-funcionario/login.php\">Entrar</a></p>");
 }
-if (!isset($_SESSION['setor_id']) || ($_SESSION['setor_id'] != 1 && $_SESSION['setor_id'] != 2)) {
-    die("Sua conta não tem autorização para entrar no sistema..<p><a href=\"http://127.0.0.1/ButacaBox/ButacaBox/src/pages/login-funcionario/login.php\">Entrar</a></p>");
+if (!isset($_SESSION['setor_id']) || !in_array($_SESSION['setor_id'], $accepted_roles)) {
+    die("Sua conta não tem autorização para entrar no sistema.<p><a href=\"http://127.0.0.1/ButacaBox/ButacaBox/src/pages/login-funcionario/login.php\">Entrar</a></p>");
 }
-
 ?>
